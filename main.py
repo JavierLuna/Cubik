14f134f210aead56d7c533b250e63bff8c72cc51
Javier Luna molina <javierlunamolina@gmail.com>
2016-12-13 00:46:32 +0100

    I’m gonna leave it running in my raspberry pi! Also forgive my english pls

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from cubik import HeuristicSolver, RubikCube
+from cubik import HeuristicSolver
+import json
 
 solver = HeuristicSolver()
 try:
 	solver.solve()
 except KeyboardInterrupt:
 	pass
-print(solver.solutions)
+
+try:
+	with open("solution.json", 'w') as file:
+		json.dump(solver.solutions, file, indent=4)
+	print(solver.solutions)
+except Exception as e:
+	with open('solution.json', 'w') as file:
+		file.write("Error :(\n")
+		file.write(str(e))
